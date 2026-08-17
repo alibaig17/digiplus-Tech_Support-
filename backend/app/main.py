4e87099f8ba67328fd32cdc26c925e5446e39ecc
alibaig17 <alibaig1704@gmail.com>
2026-08-17 22:53:13 +0530

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_origin, "http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[settings.frontend_origin, "https://digiplus-tech-support.vercel.app","http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
